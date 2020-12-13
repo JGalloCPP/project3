8d6d0f95f6144f1d9ab88be6e8d21455c9b19981
JGalloCPP <75858870+JGalloCPP@users.noreply.github.com>
2020-12-13 14:45:45 -0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,19 @@
+const todos = [
+{
+id: 1,
+text: 'Sleep early',
+completion: false
+},
+{
+id: 2,
+text: 'Do not procrasinate CIS 3110 projects',
+completion: false
+},
+{
+id: 3,
+text: 'Wake up early',
+completion: false
+}
+];
+
+export default todos;
